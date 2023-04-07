348ffc45eb69d3868fee9de50b8beef1e1868712
abralic98 <twogamers1950@gmail.com>
2023-04-07 10:19:13 +0100

    red

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 
 import { MainChat } from '@/features/main-chat'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    if (!localStorage.getItem('chaby-token')) {
+      router.push('/login')
+    }
+  })
   return (
     <>
       <Head>
